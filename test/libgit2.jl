08b6ea6e239c4d46829784e6037f73214706ab8b
kshyatt <kshyatt@physics.ucsb.edu>
2016-08-01 15:03:43 -0700

    More libgit2 badtimes

--- test/libgit2.jl
+++ test/libgit2.jl
@@ -374,6 +374,7 @@ mktempdir() do dir
             finally
                 finalize(repo)
             end
+        end
     end
 
     @testset "Fetch from cache repository" begin
@@ -563,39 +564,5 @@ mktempdir() do dir
         @test LibGit2.checkused!(creds)
         @test creds.user == creds_user
         @test creds.pass == creds_pass
-        @test LibGit2.reset!(creds) == 3
-        @test !LibGit2.checkused!(creds)
-        @test creds.count == 2
-        @test creds[:user] == creds_user
-        @test creds[:pass] == creds_pass
-        @test creds[:pubkey] === nothing
-        @test creds[:user, "localhost"] == creds_user
-        @test creds[:pubkey, "localhost"] === nothing
-        @test creds[:usesshagent, "localhost"] == "Y"
-        creds[:usesshagent, "localhost"] = "E"
-        @test creds[:usesshagent, "localhost"] == "E"
-
-        creds = LibGit2.CachedCredentials()
-        @test !LibGit2.checkused!(creds)
-        @test !LibGit2.checkused!(creds)
-        @test !LibGit2.checkused!(creds)
-        @test LibGit2.checkused!(creds)
-        @test LibGit2.reset!(creds) == 3
-        @test !LibGit2.checkused!(creds)
-        @test creds.count == 2
-        @test creds[:user, "localhost"] === nothing
-        @test creds[:pass, "localhost"] === nothing
-        @test creds[:pubkey, "localhost"] === nothing
-        @test creds[:prvkey, "localhost"] === nothing
-        @test creds[:usesshagent, "localhost"] === nothing
-        creds[:user, "localhost"] = creds_user
-        creds[:pass, "localhost"] = creds_pass
-        creds[:usesshagent, "localhost"] = "Y"
-        @test creds[:user] === nothing
-        @test creds[:user, "localhost2"] === nothing
-        @test creds[:user, "localhost"] == creds_user
-        @test creds[:pass, "localhost"] == creds_pass
-        @test creds[:pubkey, "localhost"] === nothing
-        @test creds[:usesshagent, "localhost"] == "Y"
     end
 end
